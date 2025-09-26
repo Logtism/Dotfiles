2777bdb1cfe25cbd946bcb74ab916c99485866d0
Logtism <78323275+Logtism@users.noreply.github.com>
2025-09-26 16:57:45 +1200

    Added sway config

--- install.sh
+++ install.sh
@@ -14,6 +14,14 @@ window_manager_common() {
     rm -r $CONFIG_DIR/dunst
     ln -sf $PWD/common/dunst $CONFIG_DIR/dunst
     echo "Installed dunst config"
+
+    rm -r $CONFIG_DIR/rofi
+    ln -sf $PWD/common/rofi $CONFIG_DIR/rofi
+    echo "Installed rofi config"
+
+    rm $CONFIG_DIR/xfce4/xfconf/xfce-perchannel-xml/thunar.xml
+    ln -sf $PWD/common/xfce4/xfconf/xfce-perchannel-xml/thunar.xml $CONFIG_DIR/xfce4/xfconf/xfce-perchannel-xml/thunar.xml
+    echo "Installed thunar config"
 }
 
 qtile() {
@@ -24,11 +32,33 @@ qtile() {
     echo "Installed qtile config"
 }
 
-printf "1) qtile\n> "
+sway() {
+    window_manager_common
+
+    rm $CONFIG_DIR/sway
+    ln -s $PWD/sway/sway $CONFIG_DIR/sway
+    echo "Installed sway config"
+
+    rm $CONFIG_DIR/swayidle
+    ln -s $PWD/sway/swayidle $CONFIG_DIR/swayidle
+    echo "Installed swayidle config"
+
+    rm $CONFIG_DIR/waybar
+    ln -s $PWD/sway/waybar $CONFIG_DIR/waybar
+    echo "Installed waybar config"
+
+    rm $CONFIG_DIR/xdg-desktop-portal
+    ln -s $PWD/sway/xdg-desktop-portal $CONFIG_DIR/xdg-desktop-portal
+    echo "Installed portals config"
+}
+
+printf "1) qtile 2) sway\n> "
 read option
 
 if [ $option -eq "1" ]; then
     qtile
+elif [ $option -eq "2" ]; then
+    sway
 else
     echo "$option is not a valid option."
 fi
